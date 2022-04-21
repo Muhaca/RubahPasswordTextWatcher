99923522f2fa53640b2e485fc215d8ea61bfadd6
ADI <muhamadcasdi@gmail.com>
2022-04-21 12:14:07 +0700

    Pembutan UI

---
+++ app/src/main/java/adiweb/co/id/rubahpasswordtextwatcher/MainActivity.java
@@ -0,0 +1,14 @@
+package adiweb.co.id.rubahpasswordtextwatcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
